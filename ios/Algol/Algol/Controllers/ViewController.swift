5cf32b6012c14b06a2350669ccd5e41ab3395124
William Kim <azamara@me.com>
2015-11-12 01:24:29 +0900

    feat: add update for today widget

--- ios/Algol/Algol/Controllers/ViewController.swift
+++ ios/Algol/Algol/Controllers/ViewController.swift
@@ -18,37 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var dustLabel: UILabel!
     
-    var timeLabel: UILabel?
-    
-    var timer: NSTimer?
-    let INTERVAL_SECONDS = 0.2
-    
-    var dateFormatter = NSDateFormatter()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         titleLabel.text = "Hello world!!"
 
         // Task Queue
-        let queue = TaskQueue()
-
-        queue.tasks +=~ {
-            SwaggerClientAPI.HomeAPI.homeGet(location: Double(2)).execute { (response, error) in
-                if response!.statusCode == 200,
-                    let home: Home = response!.body as Home {
-                        self.homeLabel!.text = "\(home.home_id!)"
-                        self.temperatureLabel!.text = "\(home.temperature!)º"
-                        self.humidityLabel!.text = "\(home.humidity!)%"
-                        self.dustLabel!.text = "\(home.dust!)"
-                }
-            }
-        }
-
-        queue.tasks +=! {
-        }
-
-        queue.run()
+        getSensor()
 
         
         // Alamofire
@@ -103,25 +79,6 @@ class ViewController: UIViewController {
 //        subscription.dispose()
 //        
 //        print("Main Thread Ended ----")
-        
-        
-        // Widget
-        // set up date formatter since it's expensive to keep creating them
-        self.dateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
-        self.dateFormatter.timeStyle = NSDateFormatterStyle.LongStyle
-        
-        // create and add label to display time
-        timeLabel = UILabel(frame: self.view.frame)
-        updateTimeLabel(nil) // to initialize the time displayed
-        // style the label a little: multiple lines, center, large text
-        timeLabel?.numberOfLines = 0 // allow it to wrap on to multiple lines if needed
-        timeLabel?.textAlignment = .Center
-        timeLabel?.font = UIFont.systemFontOfSize(28.0)
-        self.view.addSubview(timeLabel!)
-        
-        // Timer will tick ever 1/5 of a second to tell the label to update the display time
-        timer = NSTimer.scheduledTimerWithTimeInterval(INTERVAL_SECONDS, target: self, selector: "updateTimeLabel:", userInfo: nil, repeats: true)
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -154,21 +111,30 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateTimeLabel(timer: NSTimer!)
-    {
-        if let label = timeLabel
-        {
-            // get the current time
-            let now = NSDate()
-            // convert time to a string for display
-            let dateString = dateFormatter.stringFromDate(now)
-            label.text = dateString
-            // set the dateString in the shared data store
-            let defaults = NSUserDefaults(suiteName: "group.io.jnw.Algol.AlgolWidget")
-            defaults?.setObject("\(dateString) \n dust: \(self.dustLabel!.text!) \n temperature: \(self.temperatureLabel!.text!) \n humidity: \(self.humidityLabel!.text!)", forKey: "timeString")
-            // tell the defaults to write to disk now
-            defaults?.synchronize()
+    func getSensor() {
+        let queue = TaskQueue()
+        
+        queue.tasks +=~ {
+            SwaggerClientAPI.HomeAPI.homeGet(location: Double(2)).execute { (response, error) in
+                if response!.statusCode == 200,
+                    let home: Home = response!.body as Home {
+                        print(home)
+                        self.homeLabel!.text = "\(home.home_id!)"
+                        self.temperatureLabel!.text = "\(home.temperature!)º"
+                        self.humidityLabel!.text = "\(home.humidity!)%"
+                        self.dustLabel!.text = "\(home.dust!)"
+                }
+            }
         }
+        
+        queue.tasks +=! {
+        }
+        
+        queue.run()
+    }
+    
+    @IBAction func btnRefresh(sender: AnyObject) {
+        getSensor()
     }
 
 }
--- ios/Algol/AlgolWidget/TodayViewController.swift
+++ ios/Algol/AlgolWidget/TodayViewController.swift
@@ -14,8 +14,8 @@ import RxSwift
 class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var widgetTimeLabel: UILabel?
     
-    
     var x:Int = 1;
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,18 +28,29 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
-    func getSensor() {
-        SwaggerClientAPI.HomeAPI.homeGet(location: Double(2)).execute { (response, error) in
-            if response!.statusCode == 200,
-                let home: Home = response!.body as Home {
-                    self.homeLabel!.text = "\(home.home_id!)"
-                    self.temperatureLabel!.text = "\(home.temperature!)º"
-                    self.humidityLabel!.text = "\(home.humidity!)%"
-                    self.dustLabel!.text = "\(home.dust!)"
-                    
-                    widgetTimeLabel?.text = "\(NSDate.init()) \n dust: \(home.dust!)"
+    func getSensor(completionHandler: ((NCUpdateResult) -> Void)?=nil) {
+        widgetTimeLabel?.text = "날짜11: \n \(NSDate.init())"
+
+        let queue = TaskQueue()
+        
+        queue.tasks +=~ {
+            SwaggerClientAPI.HomeAPI.homeGet(location: Double(2)).execute { (response, error) in
+                if response!.statusCode == 200,
+                    let home: Home = response!.body as Home {
+                        print(200)
+                        //                    self.homeLabel!.text = "\(home.home_id!)"
+                        //                    self.temperatureLabel!.text = "\(home.temperature!)º"
+                        //                    self.humidityLabel!.text = "\(home.humidity!)%"
+                        //                    self.dustLabel!.text = "\(home.dust!)"
+                        self.widgetTimeLabel?.text = "날짜2: \n \(NSDate.init()) \n dust: \(home.dust!)"
+                        if (completionHandler != nil) {
+                            completionHandler!(NCUpdateResult.NewData)
+                        }
+                }
             }
         }
+        
+        queue.run()
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
@@ -48,17 +59,16 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Perform any setup necessary in order to update the view.
         widgetTimeLabel?.text = "Still not sure!!!"
 
-        self.getSensor()
+        getSensor(completionHandler)
         
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
         
-        completionHandler(NCUpdateResult.NewData)
     }
 
     @IBAction func btnRefresh(sender: AnyObject) {
-        self.getSensor()
+        getSensor()
     }
     
 }
