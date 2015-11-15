0b1efba72ba3768ee9f5ab247a561da42ea81838
William Kim <azamara@me.com>
2015-11-15 23:51:54 +0900

    feat: add refresh button

--- ios/Algol/Algol/Controllers/WeatherViewController.swift
+++ ios/Algol/Algol/Controllers/WeatherViewController.swift
@@ -16,6 +16,12 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var dustLabel: UILabel!
+    @IBOutlet weak var imgWater: UIImageView!
+    @IBOutlet weak var imgTemperature: UIImageView!
+    @IBOutlet weak var imgDust: UIImageView!
+    @IBOutlet weak var loadingWater: UIActivityIndicatorView!
+    @IBOutlet weak var loadingTemperature: UIActivityIndicatorView!
+    @IBOutlet weak var loadingDust: UIActivityIndicatorView!
 
     func getData() {
         print("START: WeatherViewController.getData")
@@ -27,6 +33,7 @@ class WeatherViewController: UIViewController {
         
         // Home
         queue.tasks +=~ {
+            print("getData")
             SwaggerClientAPI.HomeAPI.homeGet(location: Double(2)).execute { (response, error) in
                 if response!.statusCode == 200,
                     let home: Home = response!.body as Home {
@@ -37,20 +44,72 @@ class WeatherViewController: UIViewController {
                         self.humidityLabel!.text = "\(humidity)%";
                         self.dustLabel!.text = "\(dust)㎍";
                 }
-                
+                self.toggleRefreshAnimation(false)
             }
         }
         
         queue.run()
     }
     
+    func startLoading() {
+        temperatureLabel?.hidden = true
+        print("\(temperatureLabel.hidden)")
+        humidityLabel?.hidden = true
+        dustLabel?.hidden = true
+        imgWater?.hidden = true
+        imgTemperature?.hidden = true
+        imgDust?.hidden = true
+        
+        loadingWater?.startAnimating()
+        loadingTemperature?.startAnimating()
+        loadingDust?.startAnimating()
+        print("Start Loading")
+    }
+    
+    func stopLoading() {
+        temperatureLabel?.hidden = false
+        humidityLabel?.hidden = false
+        dustLabel?.hidden = false
+        imgWater?.hidden = false
+        imgTemperature?.hidden = false
+        imgDust?.hidden = false
+
+        loadingWater?.stopAnimating()
+        loadingTemperature?.stopAnimating()
+        loadingDust?.stopAnimating()
+        print("Stop Loading")
+    }
+    
+    func toggleRefreshAnimation(on: Bool) {
+        if on {
+            startLoading()
+        } else {
+            let delta: Int64 = 1 * Int64(NSEC_PER_SEC)
+            let time = dispatch_time(DISPATCH_TIME_NOW, delta)
+            
+            dispatch_after(time, dispatch_get_main_queue(), {
+                self.stopLoading()
+            });
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        toggleRefreshAnimation(true)
         getData()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    @IBAction func btnRefresh() {
+        toggleRefreshAnimation(true)
+        getData()
     }
 }
